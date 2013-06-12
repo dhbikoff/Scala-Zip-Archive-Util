a860750cbaa4cb6ae6176defb017455960456510
dhbikoff <dhbikoff@gmail.com>
2013-06-12 13:17:47 -0400

    Replaced system exits with exceptions. Removed absolute path directories from output zip file.

--- zip.scala
+++ zip.scala
@@ -2,10 +2,12 @@ import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.io.IOException
+import java.net.URI
 import java.util.zip.ZipEntry
 import java.util.zip.ZipOutputStream
 
-object zip {
+object zipUtil {
+
   def createFileList(file: File, outputFilename: String): List[String] = {
     file match {
       case file if file.isFile => {
@@ -16,33 +18,38 @@ object zip {
         file.list.toList.foldLeft(List[String]()) ((pathList: List[String], path: String) =>
           pathList ++ createFileList(new File(file, path), outputFilename))
       }
-      case _ => {
-        println("Invalid Path")
-        sys.exit(2)
-      }
+      case _ => throw new IOException("Bad path. No file or directory found.")
     }
   }
- 
-  def createZip(filesList: List[String], outputFilename: String) = {
+
+  def addFileToZipEntry(filename: String, parentPath: String, filePathsCount: Int): ZipEntry = {
+    if (filePathsCount <= 1) new ZipEntry(new File(filename).getName)
+    // use relative path to avoid adding absolute path directories
+    else new ZipEntry(new URI(parentPath).relativize(new URI(filename)).toString)
+  }
+
+  def createZip(filePaths: List[String], outputFilename: String, parentPath: String) = {
     try {
       println("Zipping...")
       val fileOutputStream = new FileOutputStream(outputFilename)
       val zipOutputStream = new ZipOutputStream(fileOutputStream)
 
       var buffer = new Array[Byte](1024)
-      filesList.foreach((name: String) => { 
+      filePaths.foreach((name: String) => { 
         println("adding " + name)
-        val zipEntry = new ZipEntry(name)
+        val zipEntry = addFileToZipEntry(name, parentPath, filePaths.size)
         zipOutputStream.putNextEntry(zipEntry)
         val inputStream = new FileInputStream(name)
 
+        // read file data into the input stream, then write it
+        // to the zip output stream
         var len = 1
-        while (len > 0) {
+        while ({var len = inputStream.read(buffer); len > 0}) {
           len = inputStream.read(buffer)
           if (len > 0)
           zipOutputStream.write(buffer, 0, len)
         }
-    
+        
         inputStream.close()
       })
 
@@ -57,15 +64,13 @@ object zip {
   }
   
   def main(args: Array[String]) = {
-    if (args.length != 2) { 
-      println("Wrong number of args. <source> <output>")
-      sys.exit(1)
-    }
+    if (args.length != 2) throw new IllegalArgumentException(
+      "Wrong number of arguments. Usage: <source> <dest>")
     val path = args(0)
     val outputFilename = args(1)
     val file = new File(path)
-    val filesList = createFileList(file, outputFilename)
-    createZip(filesList, outputFilename)
+    val filePaths = createFileList(file, outputFilename)
+    createZip(filePaths, outputFilename, path)
     println("Finished!")
   }
 }
